1d7b9817942eaab4dddba72acf0954a4c8c621d2
Vikas Raunak <vyraun@gmail.com>
2018-11-19 00:01:32 -0500

    missing moving model to s2s folder in seq2seqvis

--- setup_vis.sh
+++ setup_vis.sh
@@ -15,6 +15,7 @@ cd scripts
 python h5_to_faiss.py -states ../s2s/states.h5 -data decoder_out -output decoder.faiss -stepsize 100
 python h5_to_faiss.py -states ../s2s/states.h5 -data encoder_out -output encoder.faiss -stepsize 100
 python h5_to_faiss.py -states ../s2s/states.h5 -data cstar -output context.faiss -stepsize 100
+mv *.faiss ../s2s/
 cd ..
 sed -i -e 's/../S2Splay/model_api/processing/s2s_iwslt_ende/baseline-brnn.en-de.s154_acc_61.58_ppl_7.43_e21.pt/iwslt14.tokenized.de-en/iwslt-brnn2.s131_acc_62.71_ppl_7.74_e20.pt/g' VisServer.py
 python VisServer.py
