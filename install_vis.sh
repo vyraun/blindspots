572bd69674e3dc3a1d166201d6fe33ccc9b9eac8
Vikas Raunak <vyraun@gmail.com>
2018-11-18 23:20:44 -0500

    Update install_vis.sh

--- install_vis.sh
+++ install_vis.sh
@@ -3,12 +3,11 @@ unzip master.zip
 cd Seq2Seq-Vis-master
 source setup_cpu.sh
 source activate s2sv
-pip install torchtext=0.2.3
+pip install torchtext==0.2.3
 
 wget https://github.com/sebastianGehrmann/OpenNMT-py/archive/states_in_translation.zip
 unzip states_in_translation.zip
 mv OpenNMT-py-states_in_translation OpenNMT-py
 cd OpenNMT-py
 python setup.py install
-pip install torchtext
 cd ..
