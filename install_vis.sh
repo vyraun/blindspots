ef06728797804aee66c2f5af920645653d14f3fd
Vikas Raunak <vyraun@gmail.com>
2018-11-18 21:46:23 -0500

    install se2seq-vis toolkit

---
+++ install_vis.sh
@@ -0,0 +1,6 @@
+wget https://github.com/HendrikStrobelt/Seq2Seq-Vis/archive/master.zip
+unzip master.zip
+cd Seq2Seq-Vis-master
+source setup_cpu.sh
+cd ..
+source Seq2Seq-Vis-master/setup_onmt_custom.sh
